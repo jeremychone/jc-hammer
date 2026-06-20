e4060a56fd95fee812b9f31c1924ded971e64e9e
Jeremy Chone <jeremy.chone@gmail.com>
2026-06-19 18:12:09 -0700

    ~ init .user

--- jc.spoon/init.lua
+++ jc.spoon/init.lua
@@ -5,7 +5,51 @@ hs.application.enableSpotlightForNameSearches(true)
 -- --- Init Function
 local spoonPath = hs.spoons.resourcePath("")
 
+-- --- Initialize .user/ directory with default files if not present
+local function init_user_dir(spoonPath)
+	local userDir = spoonPath .. "/.user"
+	-- Create .user directory if it doesn't exist
+	if hs.fs.attributes(userDir) == nil then
+		hs.fs.mkdir(userDir)
+	end
+
+	-- Helper to copy file only if destination does not exist
+	local function copyIfNotExists(src, dest)
+		if hs.fs.attributes(dest) == nil then
+			-- hs.fs.copy does not exist; use io to copy file contents
+			local f_in = io.open(src, "rb")
+			if f_in then
+				local content = f_in:read("*a")
+				f_in:close()
+				local f_out = io.open(dest, "wb")
+				if f_out then
+					f_out:write(content)
+					f_out:close()
+				end
+			end
+		end
+	end
+
+	-- Copy config_default.lua to .user/config_user.lua
+	local configDefaultPath = spoonPath .. "/config_default.lua"
+	local configUserPath = userDir .. "/config_user.lua"
+	copyIfNotExists(configDefaultPath, configUserPath)
+
+	-- Copy templates/profiles.json to .user/profiles.json
+	local profilesTemplatePath = spoonPath .. "/templates/profiles.json"
+	local profilesUserPath = userDir .. "/profiles.json"
+	copyIfNotExists(profilesTemplatePath, profilesUserPath)
+
+	-- Copy templates/profile_current.json to .user/profile_current.json
+	local currentProfileTemplatePath = spoonPath .. "/templates/profile_current.json"
+	local currentProfileUserPath = userDir .. "/profile_current.json"
+	copyIfNotExists(currentProfileTemplatePath, currentProfileUserPath)
+end
+
 local function init()
+	-- Initialize .user/ directory with default files
+	init_user_dir(spoonPath)
+
 	-- Load configuration (user or default)
 	local utils = dofile(spoonPath .. "/utils.lua")
 	local config = utils.load_config(spoonPath)
